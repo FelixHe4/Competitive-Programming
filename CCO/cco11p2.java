dac671ad8e4a91de4eec3a080c4466bc8a9430c7
Felix He <felixhe.ski@outlook.com>
2018-12-01 10:46:28 -0500

    Add files via upload

--- CCO/cco11p2.java
+++ CCO/cco11p2.java
@@ -1,20 +1,33 @@
 package Unit2;
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 public class cco11p2 {
 	public static class Edge implements Comparable<Edge> {
-		int ev;
-		int cost;
-		int stat;
+		int ev, cost, hull;
 
-		public Edge(int ev, int cost, int stat) {
+		public Edge(int ev, int cost, int hull) {
 			this.ev = ev;
 			this.cost = cost;
-			this.stat = stat;
+			this.hull = hull;
 		}
 
 		public int compareTo(Edge o) {
-			return cost - o.cost;
+			if (this.cost > o.cost) {
+				return 1;
+			}
+			if (this.cost < o.cost) {
+				return -1;
+			}
+			return 0;
 		}
 	}
 
@@ -22,20 +35,20 @@ public class cco11p2 {
 	static int n;
 	static int edges;
 	static ArrayList<Edge>[] a;
-	static int[][] sto;
+	static int[][] dis;
 
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws IOException {
 		s = readInt();
 		n = readInt();
 		edges = readInt();
-		sto = new int[n + 1][s + 1];
-		a = new ArrayList[n];
-		for (int i = 0; i < n; i++) {
+		dis = new int[n + 1][s + 1];
+		a = new ArrayList[n + 1];
+		for (int i = 0; i < n + 1; i++) {
 			a[i] = new ArrayList<Edge>();
 		}
-		for (int i = 0; i < sto.length; i++) {
-			Arrays.fill(sto[i], 1 << 30);
+		for (int i = 0; i < dis.length; i++) {
+			Arrays.fill(dis[i], 1 << 30);
 		}
 		for (int i = 0; i < edges; i++) {
 			int c1 = readInt();
@@ -45,35 +58,36 @@ public class cco11p2 {
 			if (stat == 1) {
 				a[c1].add(new Edge(c2, cost, cost));
 				a[c2].add(new Edge(c1, cost, cost));
-			}
-			else {
+			} else {
 				a[c1].add(new Edge(c2, cost, 0));
 				a[c2].add(new Edge(c1, cost, 0));
 			}
 		}
 		Djikstras();
-		Arrays.sort(sto[n - 1]);
-		if (sto[n - 1][0] == 1 << 30) {
-			System.out.println(-1);
-		}
-		else {
-			System.out.println(sto[n - 1][0]);
-		}
 	}
 
 	public static void Djikstras() {
-		PriorityQueue<Edge> q = new PriorityQueue<Edge>();
-		q.add(new Edge(0, 0, 0));
-		sto[0][0] = 0;
-		while (!q.isEmpty()) {
-			Edge now = q.poll();
-			for (Edge Do : a[now.ev]) {
-				if (now.stat + Do.stat <= s && sto[now.ev][now.stat] + Do.cost < sto[Do.ev][now.stat + Do.stat]) {
-					sto[Do.ev][now.stat + Do.stat] = sto[now.ev][now.stat] + Do.cost;
-					q.add(new Edge(Do.ev, sto[Do.ev][now.stat + Do.stat], now.stat + Do.stat));
+		PriorityQueue<Edge> pq = new PriorityQueue<Edge>();
+		pq.add(new Edge(0, 0, 0));
+		dis[0][0] = 0;
+		while (!pq.isEmpty()) {
+			Edge cur = pq.poll();
+			if (cur.ev == n - 1) {
+				System.out.println(cur.cost);
+				return;
+			}
+			if (dis[cur.ev][cur.hull] < cur.cost) {
+				continue;
+			}
+			for (Edge e : a[cur.ev]) {
+				int hull = cur.hull + e.hull;
+				if (hull <= s && dis[e.ev][hull] > cur.cost + e.cost) {
+					dis[e.ev][hull] = cur.cost + e.cost;
+					pq.add(new Edge(e.ev, dis[e.ev][hull], hull));
 				}
 			}
 		}
+		System.out.println(-1);
 	}
 
 	final private static int BufferS = 1 << 16;
@@ -111,8 +125,7 @@ public class cco11p2 {
 		do {
 			ret[idx++] = c;
 			c = Read();
-		}
-		while (c != -1 && c != ' ' && c != '\n' && c != '\r');
+		} while (c != -1 && c != ' ' && c != '\n' && c != '\r');
 		return new String(ret, 0, idx);
 	}
 
@@ -126,8 +139,7 @@ public class cco11p2 {
 			c = Read();
 		do {
 			ret = ret * 10 + c - '0';
-		}
-		while ((c = Read()) >= '0' && c <= '9');
+		} while ((c = Read()) >= '0' && c <= '9');
 
 		if (neg)
 			return -ret;
@@ -144,8 +156,7 @@ public class cco11p2 {
 			c = Read();
 		do {
 			ret = ret * 10 + c - '0';
-		}
-		while ((c = Read()) >= '0' && c <= '9');
+		} while ((c = Read()) >= '0' && c <= '9');
 		if (neg)
 			return -ret;
 		return ret;
@@ -162,8 +173,7 @@ public class cco11p2 {
 
 		do {
 			ret = ret * 10 + c - '0';
-		}
-		while ((c = Read()) >= '0' && c <= '9');
+		} while ((c = Read()) >= '0' && c <= '9');
 
 		if (c == '.') {
 			while ((c = Read()) >= '0' && c <= '9') {
@@ -234,5 +244,9 @@ public class cco11p2 {
 		din.close();
 		pr.close();
 		System.exit(0);
+	}
+
+	static void printaaa() {
+		System.out.println(4957);
 	}
 }
