71b85a00c76620044df40d2f44d3f364907a1df5
Felix He <felixhe.ski@outlook.com>
2018-12-08 14:25:18 -0500

    Add files via upload

--- CCC/ccc14s4.java
+++ CCC/ccc14s4.java
@@ -1,32 +1,93 @@
 package Unit1;
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
 public class ccc14s4 {
-	static class Pair {
-		long vI, left, right, value;
-
-		public Pair(long vI0, long left0, long right0, long value0) {
-			vI = vI0;
-			left = left0;
-			right = right0;
-			value = value0;
-		}
-
-		public int compareTo(Pair a) {
-			return vI - a.vI;
-		}
-	}
+	static long[][] grid;
 
 	public static void main(String[] args) throws IOException {
 		int n = readInt();
 		int k = readInt();
+		int[][] arr = new int[n][5];
+		ArrayList<Integer> x = new ArrayList<Integer>();
+		ArrayList<Integer> y = new ArrayList<Integer>();
+		TreeSet<Integer> xx = new TreeSet<Integer>();
+		TreeSet<Integer> yy = new TreeSet<Integer>();
+		HashMap<Integer, Integer> compressX = new HashMap<Integer, Integer>();
+		HashMap<Integer, Integer> compressY = new HashMap<Integer, Integer>();
 		for (int i = 0; i < n; i++) {
-			int xL = readInt();
-			int yT = readInt();
-			int xR = readInt();
-			int yB = readInt();
-			int t = readInt();
+			int xL = readInt(), yT = readInt(), xR = readInt(), yB = readInt(), t = readInt();
+			arr[i][0] = xL;
+			arr[i][1] = yT;
+			arr[i][2] = xR;
+			arr[i][3] = yB;
+			arr[i][4] = t;
+			xx.add(xL);
+			compressX.put(xL, 0);
+			xx.add(xR);
+			compressX.put(xR, 0);
+			yy.add(yT);
+			compressY.put(yT, 0);
+			yy.add(yB);
+			compressY.put(yB, 0);
 		}
+		x.addAll(xx);
+		y.addAll(yy);
+		Collections.sort(x);
+		Collections.sort(y);
+		grid = new long[2005][2005];
+		for (int i = 1; i < compressX.size() + 1; i++) {
+			compressX.put(x.get(i - 1), i);
+		}
+		for (int i = 1; i < compressY.size() + 1; i++) {
+			compressY.put(y.get(i - 1), i);
+		}
+//		System.out.println(compressX);
+//		System.out.println(compressY);
+//		for (int i = 0; i < n; i++) {
+//			for (int j = 0; j < 4; j++) {
+//				System.out.print(arr[i][j] + " ");
+//			}
+//			System.out.println();
+//		}
+		for (int i = 0; i < n; i++) {
+			compute(compressX.get(arr[i][0]), compressY.get(arr[i][1]), compressX.get(arr[i][2]), compressY.get(arr[i][3]), arr[i][4]);
+		}
+		long t = 0;
+		for (int i = 1; i < x.size(); i++) {
+			for (int j = 1; j < y.size(); j++) {
+				grid[i][j] += grid[i - 1][j] + grid[i][j - 1] - grid[i - 1][j - 1];
+				if (grid[i][j] >= k) {
+					t += ((long) x.get(i) - x.get(i - 1)) * ((long) y.get(j) - y.get(j - 1));
+				}
+			}
+		}
+//		for (int i = 0; i < x.size(); i++) {
+//			for (int j = 0; j < y.size(); j++) {
+//				System.out.print(grid[i][j] + " ");
+//			}
+//			System.out.println();
+//		}
+		System.out.println(t);
+//		for (int i = 0; i < a.size() + 2; i++) {
+//			System.out.println(Arrays.toString(psa[i]));
+//		}
+	}
+
+	public static void compute(int x, int y, int w, int h, int v) {
+		grid[x][y] += v;
+		grid[x][h] -= v;
+		grid[w][y] -= v;
+		grid[w][h] += v;
 	}
 
 	final private static int BufferS = 1 << 16;
@@ -64,8 +125,7 @@ public class ccc14s4 {
 		do {
 			ret[idx++] = c;
 			c = Read();
-		}
-		while (c != -1 && c != ' ' && c != '\n' && c != '\r');
+		} while (c != -1 && c != ' ' && c != '\n' && c != '\r');
 		return new String(ret, 0, idx);
 	}
 
@@ -79,8 +139,7 @@ public class ccc14s4 {
 			c = Read();
 		do {
 			ret = ret * 10 + c - '0';
-		}
-		while ((c = Read()) >= '0' && c <= '9');
+		} while ((c = Read()) >= '0' && c <= '9');
 
 		if (neg)
 			return -ret;
@@ -97,8 +156,7 @@ public class ccc14s4 {
 			c = Read();
 		do {
 			ret = ret * 10 + c - '0';
-		}
-		while ((c = Read()) >= '0' && c <= '9');
+		} while ((c = Read()) >= '0' && c <= '9');
 		if (neg)
 			return -ret;
 		return ret;
@@ -115,8 +173,7 @@ public class ccc14s4 {
 
 		do {
 			ret = ret * 10 + c - '0';
-		}
-		while ((c = Read()) >= '0' && c <= '9');
+		} while ((c = Read()) >= '0' && c <= '9');
 
 		if (c == '.') {
 			while ((c = Read()) >= '0' && c <= '9') {
