e0c7117591ca7bcee0cae656947cb207abfafe26
Felix He <felixhe.ski@outlook.com>
2018-12-11 22:44:05 -0500

    Add files via upload

--- MISC/tle16c4p4.java
+++ MISC/tle16c4p4.java
@@ -1,6 +1,11 @@
 package Unit2;
-import java.io.*;
-import java.util.*;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
 public class tle16c4p4 {
 	static class Edge {
 		int ev, cost;
@@ -14,16 +19,26 @@ public class tle16c4p4 {
 	static int n, m, q;
 	static ArrayList<Edge>[] a;
 	static boolean[] vis;
-	static int max = 0;
-	static int start;
+	static long[] step1, step2, recDown, diameter, radius;
+	static long max, start, radius1, radius2;
+	static int sets;
 
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws IOException {
 		n = readInt();
 		m = readInt();
 		q = readInt();
+		max = 0;
+		radius1 = 0;
+		radius2 = 0;
 		a = new ArrayList[n + 1];
 		vis = new boolean[n + 1];
+		step1 = new long[n + 1];
+		step2 = new long[n + 1];
+		recDown = new long[n + 1];
+		diameter = new long[n + 1];
+		radius = new long[n + 1];
+		Arrays.fill(radius, Long.MAX_VALUE);
 		for (int i = 0; i < a.length; i++) {
 			a[i] = new ArrayList<Edge>();
 		}
@@ -32,44 +47,69 @@ public class tle16c4p4 {
 			a[bv].add(new Edge(ev, cost));
 			a[ev].add(new Edge(bv, cost));
 		}
+		sets = 0;
+		for (int i = 1; i <= n; i++) {
+			if (!vis[i]) {
+				// not necessarily a connected graph.
+				dfs(i, -1);
+				diameter(i, -1, 0);
+				sets++;
+			}
+		}
+		long disjoint = sets - 1;
+		for (int i = 0; i < sets; i++) {
+			if (radius[i] == radius1) {
+				radius2++;
+			} else if (radius[i] > radius1) {
+				radius1 = radius[i];
+				radius2 = 1;
+			}
+			disjoint += diameter[i];
+		}
 		if (q == 1) {
-			vis[1] = true;
-			int x = dfs(1, 0);
-			vis = new boolean[n + 1];
-			vis[x] = true;
-			int z = diameter(x, 0);
-			int extra = (n - 1) - m;
-			System.out.println(z + extra);
+			System.out.println(disjoint);
 		} else if (q == 2) {
-
-		}
-	}
-
-	static int dfs(int curr, int dist) {
-		if (dist > max) {
-			start = curr;
-			max = dist;
-		}
-		for (Edge e : a[curr]) {
-			if (!vis[e.ev]) {
-				vis[e.ev] = true;
-				dfs(e.ev, dist + e.cost);
+			if (radius2 != 1) {
+				println(radius1 + 1);
+			} else {
+				println(radius1);
 			}
 		}
-		return start;
+		exit();
 	}
 
-	static int diameter(int curr, int dist) {
-		if (dist > max) {
-			max = dist;
-		}
-		for (Edge e : a[curr]) {
-			if (!vis[e.ev]) {
-				vis[e.ev] = true;
-				diameter(e.ev, dist + e.cost);
+	static long dfs(int cur, int parent) {
+		vis[cur] = true;
+		for (Edge e : a[cur]) {
+			if (e.ev == parent) {
+				continue;
+			}
+			long rec = dfs(e.ev, cur);
+			// longest path
+			if (rec + e.cost > step1[cur]) {
+				recDown[cur] = e.ev;
+				step2[cur] = step1[cur];
+				step1[cur] = e.cost + rec;
+			} else if (rec + e.cost > step2[cur]) {
+				step2[cur] = rec + e.cost;
+				// second longest path.
 			}
 		}
-		return max;
+		return step1[cur];
+	}
+
+	static void diameter(int cur, int parent, long cost) {
+		diameter[sets] = Math.max(diameter[sets], step1[cur] + Math.max(cost, step2[cur]));
+		radius[sets] = Math.min(radius[sets], Math.max(step1[cur], cost));
+		for (Edge e : a[cur]) {
+			if (e.ev == parent) {
+				continue;
+			} else if (e.ev == recDown[cur]) {
+				diameter(e.ev, cur, e.cost + Math.max(cost, step2[cur]));
+			} else {
+				diameter(e.ev, cur, e.cost + Math.max(cost, step1[cur]));
+			}
+		}
 	}
 
 	final private static int BUFFER_SIZE = 1 << 16;
