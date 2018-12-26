e942424dc7831e515759c7b22588f342c5feff9c
Felix He <felixhe.ski@outlook.com>
2018-12-26 01:34:36 -0500

    Add files via upload

---
+++ CCC/ccc05j1.java
@@ -0,0 +1,166 @@
+package Unit1;
+
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.text.DecimalFormat;
+
+public class ccc05j1 {
+
+	public static void main(String[] args) throws IOException {
+		int day = readInt(), night = readInt(), weekend = readInt();
+		int day1 = day, night1 = night, weekend1 = weekend;
+		day -= 100;
+		day1 -= 250;
+		day = Math.max(0, day);
+		day1 = Math.max(0, day1);
+		DecimalFormat df = new DecimalFormat("#.##");
+		double cost1 = day * 0.25 + 0.15 * night + 0.2 * weekend;
+		double cost2 = day1 * 0.45 + 0.35 * night1 + 0.25 * weekend1;
+		System.out.print("Plan A costs ");
+		System.out.printf("%.2f", cost1);
+		System.out.println();
+		System.out.print("Plan B costs ");
+		System.out.printf("%.2f", cost2);
+		System.out.println();
+		cost1 = Double.valueOf(df.format(cost1));
+		cost2 = Double.valueOf(df.format(cost2));
+		if (cost1 > cost2) {
+			System.out.println("Plan B is cheapest.");
+		} else if (cost1 < cost2) {
+			System.out.println("Plan A is cheapest.");
+		} else {
+			System.out.println("Plan A and B are the same price.");
+		}
+	}
+
+	final private static int BUFFER_SIZE = 1 << 16;
+	private static DataInputStream din = new DataInputStream(System.in);
+	private static byte[] buffer = new byte[BUFFER_SIZE];
+	private static int bufferPointer = 0, bytesRead = 0;
+	static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+	public static String readLine() throws IOException {
+		byte[] buf = new byte[1000000]; // line length
+		int cnt = 0, c;
+		while ((c = Read()) != -1) {
+			if (c == '\n')
+				break;
+			buf[cnt++] = (byte) c;
+		}
+		return new String(buf, 0, cnt);
+	}
+
+	public static String read() throws IOException {
+		byte[] ret = new byte[1024];
+		int idx = 0;
+		byte c = Read();
+		while (c <= ' ') {
+			c = Read();
+		}
+		do {
+			ret[idx++] = c;
+			c = Read();
+		} while (c != -1 && c != ' ' && c != '\n' && c != '\r');
+		return new String(ret, 0, idx);
+	}
+
+	public static int readInt() throws IOException {
+		int ret = 0;
+		byte c = Read();
+		while (c <= ' ')
+			c = Read();
+		boolean neg = (c == '-');
+		if (neg)
+			c = Read();
+		do {
+			ret = ret * 10 + c - '0';
+		} while ((c = Read()) >= '0' && c <= '9');
+
+		if (neg)
+			return -ret;
+		return ret;
+	}
+
+	public static long readLong() throws IOException {
+		long ret = 0;
+		byte c = Read();
+		while (c <= ' ')
+			c = Read();
+		boolean neg = (c == '-');
+		if (neg)
+			c = Read();
+		do {
+			ret = ret * 10 + c - '0';
+		} while ((c = Read()) >= '0' && c <= '9');
+		if (neg)
+			return -ret;
+		return ret;
+	}
+
+	public static double readDouble() throws IOException {
+		double ret = 0, div = 1;
+		byte c = Read();
+		while (c <= ' ')
+			c = Read();
+		boolean neg = (c == '-');
+		if (neg)
+			c = Read();
+
+		do {
+			ret = ret * 10 + c - '0';
+		} while ((c = Read()) >= '0' && c <= '9');
+
+		if (c == '.') {
+			while ((c = Read()) >= '0' && c <= '9') {
+				ret += (c - '0') / (div *= 10);
+			}
+		}
+
+		if (neg)
+			return -ret;
+		return ret;
+	}
+
+	private static void fillBuffer() throws IOException {
+		bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+		if (bytesRead == -1)
+			buffer[0] = -1;
+	}
+
+	private static byte Read() throws IOException {
+		if (bufferPointer == bytesRead)
+			fillBuffer();
+		return buffer[bufferPointer++];
+	}
+
+	public void close() throws IOException {
+		if (din == null)
+			return;
+		din.close();
+	}
+
+	static void print(Object o) {
+		pr.print(o);
+	}
+
+	static void println(Object o) {
+		pr.println(o);
+	}
+
+	static void flush() {
+		pr.flush();
+	}
+
+	static void println() {
+		pr.println();
+	}
+
+	static void exit() throws IOException {
+		din.close();
+		pr.close();
+		System.exit(0);
+	}
+}
---
+++ CCC/ccc05j3.java
@@ -0,0 +1,171 @@
+package Unit1;
+
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Stack;
+
+public class ccc05j3 {
+
+	public static void main(String[] args) throws IOException {
+		Stack<String> Letter = new Stack<String>();
+		Stack<String> Word = new Stack<String>();
+		String l = readLine();
+		String place = readLine();
+		while (!place.equals("SCHOOL")) {
+			Letter.push(l);
+			Word.push(place);
+			l = readLine();
+			place = readLine();
+		}
+		Letter.push(l);
+		// System.out.println(Letter);
+		while (Letter.size() != 0) {
+			if (!Word.isEmpty()) {
+				String s = Letter.pop();
+				if (s.equals("R")) {
+					System.out.println("Turn LEFT onto " + Word.pop() + " street.");
+				} else if (s.equals("L")) {
+					System.out.println("Turn RIGHT onto " + Word.pop() + " street.");
+				}
+			} else {
+				if (Letter.pop().equals("R")) {
+					System.out.println("Turn LEFT into your HOME.");
+				} else {
+					System.out.println("Turn RIGHT into your HOME.");
+				}
+				return;
+			}
+		}
+	}
+
+	final private static int BUFFER_SIZE = 1 << 16;
+	private static DataInputStream din = new DataInputStream(System.in);
+	private static byte[] buffer = new byte[BUFFER_SIZE];
+	private static int bufferPointer = 0, bytesRead = 0;
+	static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+	public static String readLine() throws IOException {
+		byte[] buf = new byte[1000000]; // line length
+		int cnt = 0, c;
+		while ((c = Read()) != -1) {
+			if (c == '\n')
+				break;
+			buf[cnt++] = (byte) c;
+		}
+		return new String(buf, 0, cnt);
+	}
+
+	public static String read() throws IOException {
+		byte[] ret = new byte[1024];
+		int idx = 0;
+		byte c = Read();
+		while (c <= ' ') {
+			c = Read();
+		}
+		do {
+			ret[idx++] = c;
+			c = Read();
+		} while (c != -1 && c != ' ' && c != '\n' && c != '\r');
+		return new String(ret, 0, idx);
+	}
+
+	public static int readInt() throws IOException {
+		int ret = 0;
+		byte c = Read();
+		while (c <= ' ')
+			c = Read();
+		boolean neg = (c == '-');
+		if (neg)
+			c = Read();
+		do {
+			ret = ret * 10 + c - '0';
+		} while ((c = Read()) >= '0' && c <= '9');
+
+		if (neg)
+			return -ret;
+		return ret;
+	}
+
+	public static long readLong() throws IOException {
+		long ret = 0;
+		byte c = Read();
+		while (c <= ' ')
+			c = Read();
+		boolean neg = (c == '-');
+		if (neg)
+			c = Read();
+		do {
+			ret = ret * 10 + c - '0';
+		} while ((c = Read()) >= '0' && c <= '9');
+		if (neg)
+			return -ret;
+		return ret;
+	}
+
+	public static double readDouble() throws IOException {
+		double ret = 0, div = 1;
+		byte c = Read();
+		while (c <= ' ')
+			c = Read();
+		boolean neg = (c == '-');
+		if (neg)
+			c = Read();
+
+		do {
+			ret = ret * 10 + c - '0';
+		} while ((c = Read()) >= '0' && c <= '9');
+
+		if (c == '.') {
+			while ((c = Read()) >= '0' && c <= '9') {
+				ret += (c - '0') / (div *= 10);
+			}
+		}
+
+		if (neg)
+			return -ret;
+		return ret;
+	}
+
+	private static void fillBuffer() throws IOException {
+		bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+		if (bytesRead == -1)
+			buffer[0] = -1;
+	}
+
+	private static byte Read() throws IOException {
+		if (bufferPointer == bytesRead)
+			fillBuffer();
+		return buffer[bufferPointer++];
+	}
+
+	public void close() throws IOException {
+		if (din == null)
+			return;
+		din.close();
+	}
+
+	static void print(Object o) {
+		pr.print(o);
+	}
+
+	static void println(Object o) {
+		pr.println(o);
+	}
+
+	static void flush() {
+		pr.flush();
+	}
+
+	static void println() {
+		pr.println();
+	}
+
+	static void exit() throws IOException {
+		din.close();
+		pr.close();
+		System.exit(0);
+	}
+}
