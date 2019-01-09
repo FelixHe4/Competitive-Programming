706fe9297932a2e0ef02a5fdb29ae29286679902
Felix He <felixhe.ski@outlook.com>
2019-01-09 12:38:38 -0500

    Add files via upload

---
+++ MISC/mec16p3.java
@@ -0,0 +1,181 @@
+package Unit2;
+
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+public class mec16p3 {
+	static class Pair {
+		int a, b;
+
+		public Pair(int a, int b) {
+			this.a = a;
+			this.b = b;
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) throws IOException {
+		int n = readInt(), t = readInt();
+		ArrayList<Pair>[] p = new ArrayList[n];
+		for (int i = 0; i < n; i++) {
+			p[i] = new ArrayList<Pair>();
+		}
+		int[] dp1 = new int[t + 1];
+		int[] dp2 = new int[t + 1];
+		for (int j = 0; j < n; j++) {
+			int l = readInt();
+			int cost = 0, value = 0;
+			for (int i = 0; i < l; i++) {
+				cost += readInt();
+				value += readInt();
+				p[j].add(new Pair(cost, value));
+			}
+		}
+		for (int i = 0; i < p.length; i++) {
+			for (Pair pair : p[i]) {
+				for (int j = 1; j <= t; j++) {
+					// dp2[j] = dp1[j];
+					if (j >= pair.a) {
+						dp2[j] = Math.max(dp2[j], pair.b + dp1[j - pair.a]);
+					}
+				}
+			}
+			for (int j = 0; j <= t; j++) {
+				dp1[j] = dp2[j];
+			}
+		}
+		System.out.println(dp2[t]);
+	}
+
+	final private static int BUFFER_SIZE = 1 << 16;
+	private static DataInputStream din = new DataInputStream(System.in);
+	private static byte[] buffer = new byte[BUFFER_SIZE];
+	private static int bufferPointer = 0, bytesRead = 0;
+	static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+	public static String readLine() throws IOException {
+		byte[] buf = new byte[1000000]; // line length
+		int cnt = 0, c;
+		while ((c = Read()) != -1) {
+			if (c == '\n')
+				break;
+			buf[cnt++] = (byte) c;
+		}
+		return new String(buf, 0, cnt);
+	}
+
+	public static String read() throws IOException {
+		byte[] ret = new byte[1024];
+		int idx = 0;
+		byte c = Read();
+		while (c <= ' ') {
+			c = Read();
+		}
+		do {
+			ret[idx++] = c;
+			c = Read();
+		} while (c != -1 && c != ' ' && c != '\n' && c != '\r');
+		return new String(ret, 0, idx);
+	}
+
+	public static int readInt() throws IOException {
+		int ret = 0;
+		byte c = Read();
+		while (c <= ' ')
+			c = Read();
+		boolean neg = (c == '-');
+		if (neg)
+			c = Read();
+		do {
+			ret = ret * 10 + c - '0';
+		} while ((c = Read()) >= '0' && c <= '9');
+
+		if (neg)
+			return -ret;
+		return ret;
+	}
+
+	public static long readLong() throws IOException {
+		long ret = 0;
+		byte c = Read();
+		while (c <= ' ')
+			c = Read();
+		boolean neg = (c == '-');
+		if (neg)
+			c = Read();
+		do {
+			ret = ret * 10 + c - '0';
+		} while ((c = Read()) >= '0' && c <= '9');
+		if (neg)
+			return -ret;
+		return ret;
+	}
+
+	public static double readDouble() throws IOException {
+		double ret = 0, div = 1;
+		byte c = Read();
+		while (c <= ' ')
+			c = Read();
+		boolean neg = (c == '-');
+		if (neg)
+			c = Read();
+
+		do {
+			ret = ret * 10 + c - '0';
+		} while ((c = Read()) >= '0' && c <= '9');
+
+		if (c == '.') {
+			while ((c = Read()) >= '0' && c <= '9') {
+				ret += (c - '0') / (div *= 10);
+			}
+		}
+
+		if (neg)
+			return -ret;
+		return ret;
+	}
+
+	private static void fillBuffer() throws IOException {
+		bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+		if (bytesRead == -1)
+			buffer[0] = -1;
+	}
+
+	private static byte Read() throws IOException {
+		if (bufferPointer == bytesRead)
+			fillBuffer();
+		return buffer[bufferPointer++];
+	}
+
+	public void close() throws IOException {
+		if (din == null)
+			return;
+		din.close();
+	}
+
+	static void print(Object o) {
+		pr.print(o);
+	}
+
+	static void println(Object o) {
+		pr.println(o);
+	}
+
+	static void flush() {
+		pr.flush();
+	}
+
+	static void println() {
+		pr.println();
+	}
+
+	static void exit() throws IOException {
+		din.close();
+		pr.close();
+		System.exit(0);
+	}
+}
