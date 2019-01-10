f132db6d712cf7ff990cadf9af2f1c5875f4b811
Felix He <felixhe.ski@outlook.com>
2019-01-10 10:47:10 -0500

    Add files via upload

--- MISC/BubbleSort.java
+++ MISC/BubbleSort.java
@@ -1,27 +1,32 @@
 package Unit2;
-import java.util.*;
+import java.util.Scanner;
 public class BubbleSort {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int N = scan.nextInt();
-		int[] A = new int[N];
-		// manually do the code to sort
+		int[] arr = new int[N];
 		for (int i = 0; i < N; i++) {
-			boolean swap = false;
-			for (int j = i; j < N - 1; j++) {
-				if (A[j] > A[j + 1]) {
-					// swap
-					int temp = A[j + 1];
-					A[j + 1] = A[j];
-					A[j] = temp;
+			arr[i] = scan.nextInt();
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N - i - 1; j++) {
+				boolean swap = false;
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
 					swap = true;
 				}
-			}
-			if (swap) {
-				break;
+				if (swap) {
+					for (int k = 0; k < N; k++) {
+						System.out.print(arr[k] + " ");
+					}
+					System.out.println();
+				}
 			}
 		}
 	}
-
 }
