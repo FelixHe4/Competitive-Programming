7c1ac6375ac5332cbbd46da50717dddbaeb0e0b7
Felix He <felixhe.ski@outlook.com>
2019-01-18 14:15:49 -0500

    Add files via upload

--- DataStructures/BBSTAvlTree.java
+++ DataStructures/BBSTAvlTree.java
@@ -7,31 +7,29 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 
 public class BBSTAvlTree {
-	// redid Jeffrey Xiao's template
-	Node root;
+	// rocoded Jeffrey Xiao's template
+	static Node root;
 
 	public static class Node {
-		int key, value, height;
+		int key, height, size;
 		Node left, right;
 
-		public Node(int key, int value) {
+		public Node(int key, int height, int size) {
 			this.key = key;
-			this.value = value;
-			this.height = 0;
-		}
-
-		public Node(int key) {
-			this.key = key;
-			this.value = key;
-			this.height = 0;
+			this.height = height;
+			this.size = size;
 		}
 	}
 
-	public static void resetHeight(Node n) {
+	public static void reset(Node n) {
+		n.size = getSize(n.left) + getSize(n.right) + 1;
+		// System.out.println(getSize(n.left));
 		n.height = Math.max(getHeight(n.left), getHeight(n.right)) + 1;
 	}
 	// get the higher one's height + 1 for your current node's height.
 
+	// get the higher one's height + 1 for your current node's height.
+	// returns the height of the node
 	public static int getHeight(Node n) {
 		if (n == null) {
 			return -1;
@@ -40,11 +38,46 @@ public class BBSTAvlTree {
 		}
 	}
 
+	// returns the size of the node (amount of nodes under it)
+	public static int getSize(Node n) {
+		if (n == null) {
+			return 0;
+		} else {
+			return n.size;
+		}
+	}
+
+	// get the value in the node
+	public static int getKey(Node n) {
+		if (n == null) {
+			return -1;
+		} else {
+			return n.key;
+		}
+	}
+
 	// traverse from a given node.
 	public static void traverse(Node n) {
 		if (n != null) {
 			traverse(n.left);
+			print(n.key + " ");
 			traverse(n.right);
+		}
+		return;
+	}
+
+	// get the element at index
+	public static int smallestElement(Node n, int index) {
+		if (n != null) {
+			if (getSize(n.left) == index) {
+				return n.key;
+			} else if (getSize(n.left) > index) {
+				return smallestElement(n.left, index);
+			} else {
+				return smallestElement(n.right, index - getSize(n.left) - 1);
+			}
+		} else {
+			return -1;
 		}
 	}
 
@@ -70,22 +103,23 @@ public class BBSTAvlTree {
 			} else if (k < n.key) {
 				return get(n.left, k);
 			}
-			return n.value;
+			return n.key;
 		} else {
 			return 0;
 		}
 	}
 
-	public static Node add(Node n, int k, int v) {
+	// add a node into the tree
+	public static Node add(Node n, int k) {
 		if (n == null) {
-			return new Node(k, v);
+			return new Node(k, 0, 1);
+			// node's initial height is 0, size is 1 (itself)
+			// rebalance/reset will change the values into proper values
 		} else {
-			if (v > n.key) {
-				n.right = add(n.right, k, v);
-			} else if (v < n.key) {
-				n.left = add(n.left, k, v);
+			if (k < n.key) {
+				n.left = add(n.left, k);
 			} else {
-				n.key = v;
+				n.right = add(n.right, k);
 			}
 			return balance(n);
 		}
@@ -101,34 +135,43 @@ public class BBSTAvlTree {
 			n.left = remove(n.left, k);
 		} else {
 			// this node to remove;
+			// this node to remove;
 			if (n.left == null) {
-				n = n.right;
-				return n;
+				return n.right;
 			} else if (n.right == null) {
-				n = n.left;
-				return n;
+				return n.left;
 			} else {
-				Node replace = min(n.right);
-				n.key = replace.key;
-				n.value = replace.value;
-				n.right = remove(n.right, n.key);
-				return n;
+				Node replace = n;
+				n = min(replace.right);
+				n.right = minR(replace.right);
+				n.left = replace.left;
 			}
 		}
 		return balance(n);
 	}
 
 	// finds the lowest left most node.
-	public static Node min(Node n) {
-		while (n.left != null) {
-			n = n.left;
+	public static Node minR(Node n) {
+		if (n.left == null) {
+			return n.right;
+		} else {
+			n.left = minR(n.left);
+			return balance(n);
 		}
-		return n;
 	}
 
-//method used to rebalance the BST
+	public static Node min(Node n) {
+		if (n.left == null) {
+			return n;
+		} else {
+			return min(n.left);
+		}
+	}
+
+	// balance needs to be reset when the heights of a subtrees of a given node differ by more than 1
+	// method used to rebalance the BST
 	public static Node balance(Node n) {
-		resetHeight(n);
+		reset(n);
 		// first reset the height of n
 		int diff1 = getHeight(n.left) - getHeight(n.right);
 		// get the difference between the heights of the two subtrees
@@ -157,8 +200,8 @@ public class BBSTAvlTree {
 		Node x = n.right;
 		n.right = x.left;
 		x.left = n;
-		resetHeight(n);
-		resetHeight(x);
+		reset(n);
+		reset(x);
 		return x;
 	}
 
@@ -166,13 +209,38 @@ public class BBSTAvlTree {
 		Node x = n.left;
 		n.left = x.right;
 		x.right = n;
-		resetHeight(n);
-		resetHeight(x);
+		reset(n);
+		reset(x);
 		return x;
 	}
 
 	public static void main(String[] args) throws IOException {
-
+		// root = remove(root, k);
+		// contains(root, k);
+		// get(root, k);
+		// add(root, k, v);
+		// add(root, k, k);
+		int n = readInt(), m = readInt();
+		for (int i = 0; i < n; i++) {
+			int x = readInt();
+			root = add(root, x);
+		}
+		int lastAns = 0;
+		while (m-- > 0) {
+			String s = read();
+			int v = readInt() ^ lastAns;
+			if (s.equals("I")) {
+				root = add(root, v);
+			} else if (s.equals("R")) {
+				root = remove(root, v);
+			} else if (s.equals("S")) {
+				lastAns = smallestElement(root, v);
+				System.out.println(lastAns);
+			} else {
+				lastAns = get(root, v);
+				System.out.println(lastAns);
+			}
+		}
 	}
 
 	final private static int BUFFER_SIZE = 1 << 16;
