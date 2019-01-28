cec87eaba64b1b6b74e30efdec277f243274c4aa
Felix He <felixhe.ski@outlook.com>
2019-01-28 10:57:12 -0500

    Add files via upload

---
+++ CCO/cco07p3.java
@@ -0,0 +1,208 @@
+package Unit2;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+public class cco07p3 {
+
+	public static void main(String[] args) throws IOException {
+		int t = readInt();
+		for (int runs = 0; runs < t; runs++) {
+			int n = readInt(); // Number of bowling pins
+			int k = readInt(); // Number of bowling balls
+			int w = readInt(); // The width of the bowling ball
+			int[] pins = new int[n + (w) * 2];
+			for (int i = 1; i <= n; i++) {
+				pins[i] = readInt();
+			}
+			int[] psa = new int[n + w * 2];
+			psa = pins;
+			for (int i = 1; i < n + w; i++) {
+				psa[i] += psa[i - 1];
+			}
+			int[][] dp = new int[600][10500];
+			for (int i = 1; i <= k; i++) {
+				// current position
+				for (int j = 1; j <= n + w; j++) {
+					int cur = j - w > 0 ? j - w : 0;
+					dp[i][j] = Math.max(dp[i - 1][cur] + psa[j] - psa[cur], dp[i][j - 1]);
+					if (i >= 2) {
+						// larger row size
+						for (int l = 1; l < w; l++) {
+							if (cur - l >= 0) {
+								dp[i][j] = Math.max(dp[i][j], dp[i - 2][cur - l] + psa[j] - psa[cur - l]);
+							} else {
+								break;
+							}
+						}
+					}
+				}
+			}
+			int max = -(1 << 30);
+			for (int i = 0; i < n + w; i++) {
+				max = Math.max(max, dp[k][i]);
+			}
+			System.out.println(max);
+		}
+	}
+
+	final private static int BufferS = 1 << 16;
+	private static DataInputStream din = new DataInputStream(System.in);
+	private static byte[] buffer = new byte[BufferS];
+	private static int bufferPointer = 0, bytesRead = 0;
+	static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+
+	public static String readLine() throws IOException {
+		byte[] buf = new byte[64];
+		int cnt = 0, c;
+		while ((c = Read()) != -1) {
+			if (c == '\n')
+				break;
+			buf[cnt++] = (byte) c;
+		}
+		return new String(buf, 0, cnt);
+	}
+
+	public static String read() throws IOException {
+		byte[] ret = new byte[1024];
+		int idx = 0;
+		byte c = Read();
+		while (c <= ' ') {
+			c = Read();
+		}
+		do {
+			ret[idx++] = c;
+			c = Read();
+		} while (c != -1 && c != ' ' && c != '\n' && c != '\r');
+		return new String(ret, 0, idx);
+	}
+
+	public static int readInt() throws IOException {
+		int ret = 0;
+		byte c = Read();
+		while (c <= ' ')
+			c = Read();
+		boolean neg = (c == '-');
+		if (neg)
+			c = Read();
+		do {
+			ret = ret * 10 + c - '0';
+		} while ((c = Read()) >= '0' && c <= '9');
+
+		if (neg)
+			return -ret;
+		return ret;
+	}
+
+	public static long readLong() throws IOException {
+		long ret = 0;
+		byte c = Read();
+		while (c <= ' ')
+			c = Read();
+		boolean neg = (c == '-');
+		if (neg)
+			c = Read();
+		do {
+			ret = ret * 10 + c - '0';
+		} while ((c = Read()) >= '0' && c <= '9');
+		if (neg)
+			return -ret;
+		return ret;
+	}
+
+	public static double readDouble() throws IOException {
+		double ret = 0, div = 1;
+		byte c = Read();
+		while (c <= ' ')
+			c = Read();
+		boolean neg = (c == '-');
+		if (neg)
+			c = Read();
+
+		do {
+			ret = ret * 10 + c - '0';
+		} while ((c = Read()) >= '0' && c <= '9');
+
+		if (c == '.') {
+			while ((c = Read()) >= '0' && c <= '9') {
+				ret += (c - '0') / (div *= 10);
+			}
+		}
+
+		if (neg)
+			return -ret;
+		return ret;
+	}
+
+	static char readChar() throws IOException {
+		return next().charAt(0);
+	}
+
+	static float readFloat() throws IOException {
+		return Float.parseFloat(next());
+	}
+
+	static boolean readBool() throws IOException {
+		return Boolean.parseBoolean(next());
+	}
+
+	static short readShort() throws IOException {
+		return Short.parseShort(next());
+	}
+
+	static byte readByte() throws IOException {
+		return Byte.parseByte(next());
+	}
+
+	private static void fillBuffer() throws IOException {
+		bytesRead = din.read(buffer, bufferPointer = 0, BufferS);
+		if (bytesRead == -1)
+			buffer[0] = -1;
+	}
+
+	private static byte Read() throws IOException {
+		if (bufferPointer == bytesRead)
+			fillBuffer();
+		return buffer[bufferPointer++];
+	}
+
+	public void close() throws IOException {
+		if (din == null)
+			return;
+		din.close();
+	}
+
+	static void print(Object o) {
+		pr.print(o);
+	}
+
+	static void println(Object o) {
+		pr.println(o);
+	}
+
+	static void flush() {
+		pr.flush();
+	}
+
+	static void println() {
+		pr.println();
+	}
+
+	static void exit() throws IOException {
+		din.close();
+		pr.close();
+		System.exit(0);
+	}
+}
